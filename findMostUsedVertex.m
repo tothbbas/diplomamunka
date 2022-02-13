74eeb6ce98fd4857afd45796c776d9c633cc4539
DESKTOP-V50D9CP\Felhasználó <tothbbas@gmail.com>
2022-02-13 18:44:52 +0100

    Find the vertex that most faces use

---
+++ findMostUsedVertex.m
@@ -0,0 +1,20 @@
+function idx = findMostUsedVertex(F,n)
+% A legtöbbet használt vertex megkeresése
+%   F : lapok mátrixa, n * 3
+if nargin > 2
+    number_of_vertices = n;
+else
+    number_of_vertices = max(max(F));
+end
+
+vertex_uses = zeros(1,number_of_vertices);
+for i = 1:number_of_vertices
+    idx = (F == i);
+    vertex_uses(i) = sum(idx(:));
+end
+
+disp(vertex_uses)
+[~,idx] = max(vertex_uses);
+
+end
+
