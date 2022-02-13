c2e84424a0cfd461e61fcce54f701686762413ac
DESKTOP-V50D9CP\Felhasználó <tothbbas@gmail.com>
2022-02-13 22:29:11 +0100

    display object file from vertices and faces

---
+++ displayObj.m
@@ -0,0 +1,23 @@
+function displayObj(V,F,color)
+%   obj file megjelenítése a vertexek, illetve a lapok alapján
+%   V : vertexek
+%   F : lapok
+%   color : szín (opcionális)
+if nargin > 2
+    c = color;
+else
+    c = [0.5765, 0.7412, 0.5490];
+end
+X = V(:,1);
+Y = V(:,2);
+Z = V(:,3);
+trisurf(F,X,Y,Z,'FaceColor',c);
+lighting phong;
+camproj('perspective');
+axis square; 
+axis off;
+axis equal
+axis tight;
+
+end
+
