048488fb8d34076fdc66f265eb54baf39890627a
DESKTOP-V50D9CP\Felhasználó <tothbbas@gmail.com>
2022-02-18 22:08:14 +0100

    List vertex neighbors in correct order

--- getVertexNeighbors.m
+++ getVertexNeighbors.m
@@ -1,13 +1,37 @@
 function neighbors = getVertexNeighbors(vertex_idx,F)
-% Megadja egy adott vertex_idx -ű vertex F lapok által meghatározott szomszédjait
+% Megadja egy adott vertex_idx -ű vertex F lapok által meghatározott
+% szomszédjait helyes körbejárási sorrendben
 %   F : lapok (faces)
 %   vertex_idx : adott indexű vertex
 
-% megnézzük, F melyik sorában van benne a vertex_idx -ű vertex, (ahol a sum
-% nagyobb, mint 0), majd vesszük ezeket a sorokat és unique vektort
-% készítünk az elemeiből. illetve eltávolítjuk belőle a vertex_idx -ű
-% elemet
 u = unique(F(sum(F == vertex_idx, 2) > 0, :));
-neighbors = u(u~=vertex_idx);
+edges_num = length(u(u~=vertex_idx));
+neighbor_edges = zeros(edges_num,2);
+counter = 1;
+
+for i = 1:length(F)
+    v = F(i,:);
+    if sum(v(vertex_idx == v)) > 0
+        neighbor_edges(counter,:) = v(v ~= vertex_idx);
+        counter = counter + 1;
+    end
+end
+
+neighbors = zeros(length(neighbor_edges),1);
+neighbors(1) = neighbor_edges(1,1);
+row = 1;
+other = 2;
+counter = 2;
+while min(size(neighbor_edges)) == 2
+    element = neighbor_edges(row,other);
+    neighbors(counter) = element;
+    neighbor_edges(row,:) = [];
+    [row,col] = find(neighbor_edges == element,1,'first');
+    if col == 1
+        other = 2;
+    else
+        other = 1;
+    end
+    counter = counter + 1;
 end
 
